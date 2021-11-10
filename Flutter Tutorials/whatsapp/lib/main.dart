3e5102a44bcbe38720ebfb304055df07c2a94a76
Usama Sarwar <UsamaSarwarOfficial@gmail.com>
2021-11-10 17:23:22 +0500

    [+] TabBar

--- Flutter Tutorials/whatsapp/lib/main.dart
+++ Flutter Tutorials/whatsapp/lib/main.dart
@@ -7,15 +7,49 @@ void main() {
       title: 'WhatsApp',
       theme: ThemeData(
         primarySwatch: Colors.teal,
+        primaryColor: Colors.teal[800],
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('WhatsApp'),
-        ),
-        body: Center(
-          child: ElevatedButton(
-            onPressed: () {},
-            child: const Text('Sample'),
+      home: DefaultTabController(
+        initialIndex: 1,
+        length: 4,
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.teal[800],
+            title: const Text('WhatsApp'),
+            actions: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.search),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.more_vert),
+              )
+            ],
+            bottom: const TabBar(
+              tabs: <Widget>[
+                Tab(icon: Icon(Icons.camera_alt)),
+                Tab(text: 'CHATS'),
+                Tab(text: 'STAUS'),
+                Tab(text: 'CALLS'),
+              ],
+            ),
+          ),
+          body: const TabBarView(
+            children: <Widget>[
+              Center(
+                child: Text("It's cloudy here"),
+              ),
+              Center(
+                child: Text("It's rainy here"),
+              ),
+              Center(
+                child: Text("It's sunny here"),
+              ),
+              Center(
+                child: Text("It's sunny here"),
+              ),
+            ],
           ),
         ),
       ),
